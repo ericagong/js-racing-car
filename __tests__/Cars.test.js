60b89e069ea06ffe1cfaf2271d373e38545b1047
Erica Gong <the.erica.gong@gmail.com>
2024-09-04 08:57:37 +0900

    refactor: 많은 역할을 하던 Game 객체로부터 Car 배열 관리하는 역할을 Cars 객체로  분리

---
+++ __tests__/Cars.test.js
@@ -0,0 +1,64 @@
+import { Cars } from '../src/Models/Cars';
+
+describe('CarNames의 유효성을 확인하고, 유효할 경우만 Car 배열을 생성한다.', () => {
+    // CHECK 테스트 코드를 위해 public으로 빼는게 맞는지?
+    const CARS_ERROR_MESSAGE = Cars.ERROR_MESSAGE;
+
+    it.each([
+        { carNames: ['erica', 'erica'] },
+        { carNames: ['gong0', 'gong0'] },
+        { carNames: ['1031', '1031'] },
+        { carNames: ['*****', '*****'] },
+        { carNames: ['*e*1C', '*e*1C'] },
+        { carNames: [' ', ' '] },
+    ])(
+        'CarNames에 중복된 Car 이름이 존재하면, 에러를 발생시킨다.',
+        ({ carNames }) => {
+            expect(() => Cars.from(carNames)).toThrow(
+                CARS_ERROR_MESSAGE.DUPLICATED_NAME,
+            );
+        },
+    );
+
+    it.each([
+        { carNames: ['erica', 'Erica'] },
+        { carNames: ['gong0', 'Gong0', '1031', '1031!', '*****'] },
+        { carNames: ['*e*1C', '*e*1c', 'ERICA', 'Pan', 'theon'] },
+    ])(
+        'CarNames에 중복된 Car 이름이 존재하지 않으면, Car 배열을 생성한다.',
+        ({ carNames }) => {
+            expect(() => Cars.from(carNames)).not.toThrow();
+            expect(Cars.from(carNames)).toHaveLength(carNames.length);
+        },
+    );
+
+    it.each([
+        { carNames: ['erica', 'Erica'] },
+        { carNames: ['gong0', 'Gong0', '1031', '1031!', '*****'] },
+        { carNames: ['*e*1C', '*e*1c', 'ERICA', 'Pan', 'theon'] },
+    ])('유효한 CarNames 배열로, Car 배열을 생성한다.', ({ carNames }) => {
+        const cars = Cars.from(carNames);
+
+        expect(cars).toHaveLength(carNames.length);
+        cars.forEach((car) => {
+            expect(car.getRecord()).toEqual({ name: car.name, position: 0 });
+        });
+    });
+});
+
+// TODO playOneRound 테스트 함수 작성
+describe('playOneRound 테스트 함수 작성', () => {});
+
+it.each([
+    { carNames: ['erica', 'Erica'] },
+    { carNames: ['gong0', 'Gong0', '1031', '1031!', '*****'] },
+    { carNames: ['*e*1C', '*e*1c', 'ERICA', 'Pan', 'theon'] },
+])('자동차 배열에 속한 모든 자동차의 정보를 반환한다.', ({ carNames }) => {
+    const cars = Cars.from(carNames);
+    const roundRecord = Cars.getRoundRecord(cars);
+
+    expect(roundRecord).toHaveLength(carNames.length);
+    expect(roundRecord).toEqual(
+        carNames.map((carName) => ({ name: carName, position: 0 })),
+    );
+});
---
+++ src/Models/Cars.js
@@ -0,0 +1,47 @@
+import Car from './Car';
+import { getRandomIntRangeOf } from '../utils';
+
+export const Cars = (function () {
+    const CAR_MOVE_CRITERIA = 4;
+    const ERROR_MESSAGE = Object.freeze({
+        DUPLICATE_CAR_NAME:
+            '중복된 자동차 이름으로는 프로그램이 동작할 수 없습니다.',
+    });
+
+    function hasDuplicatedCarNames(carNames) {
+        return new Set(carNames).size !== carNames.length;
+    }
+
+    function validateDuplicateCarNames(carNames) {
+        if (hasDuplicatedCarNames(carNames))
+            throw new Error(ERROR_MESSAGE.DUPLICATE_CAR_NAME);
+    }
+
+    function from(carNames) {
+        validateDuplicateCarNames(carNames);
+
+        return carNames.map((carName) => Car.of(carName));
+    }
+
+    // TODO 랜덤 숫자 생성 외부로 분리
+    function playOneRound(cars) {
+        cars.forEach((car) => {
+            const randomNumber = getRandomIntRangeOf(0, 9);
+            if (randomNumber >= CAR_MOVE_CRITERIA) {
+                car.move();
+            }
+        });
+    }
+
+    function getRoundRecord(cars) {
+        return cars.map((car) => car.getRecord());
+    }
+
+    return {
+        // CHECK 테스트 코드를 위해 public으로 빼는게 맞는지?
+        ERROR_MESSAGE,
+        from,
+        playOneRound,
+        getRoundRecord,
+    };
+})();
